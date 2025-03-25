970a7ffee8567855e531b5b03e2887aff6fba372
Gavin Robey <gavin.robey15@gmail.com>
2025-03-24 20:50:40 -0600

    docker set up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,12 @@
-const a = 1;
-const b = 5;
+import express from 'express';
 
-console.log(a + b);
+const app = express();
+const port = process.env.SERVER_PORT || 8000
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello from the server another test yippie hello world</h1>")
+})
+
+app.listen(port, ()=> {
+    console.log("The Server is running on http://localhost8000")
+});
